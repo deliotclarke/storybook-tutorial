46f1f26590f97c47887b8058236b600ed3ddfdbe
Daniel Clarke <deliotclarke@gmail.com>
2019-10-08 11:59:34 -0500

    added edge case story of title that's too long.

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -19,8 +19,16 @@ export const actions = {
   onArchiveTask: action('onArchiveTask'),
 };
 
-storiesOf('Task', module)
-  .addDecorator(withKnobs)
-  .add('default', () => <Task task={object("task", { ...task })} {...actions} />)
-  .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
-  .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not`;
+
+storiesOf("Task", module)
+  .add("default", () => <Task task={task} {...actions} />)
+  .add("pinned", () => (
+    <Task task={{ ...task, state: "TASK_PINNED" }} {...actions} />
+  ))
+  .add("archived", () => (
+    <Task task={{ ...task, state: "TASK_ARCHIVED" }} {...actions} />
+  ))
+  .add("long title", () => (
+    <Task task={{ ...task, title: longTitle }} {...actions} />
+  ));
